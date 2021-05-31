035832145dbecc88e412821891fb507bbdb18042
Miriamolson73 <54155622+Miriamolson73@users.noreply.github.com>
2021-05-31 11:41:31 -0300

    segunda version de ejericicio

--- ej2-7/ejercicio2-7.js
+++ ej2-7/ejercicio2-7.js
@@ -8,8 +8,24 @@ function getProcessedData(url) {
       });
   }
 
+  // segunda version
+  async function getProcessedData(url) {
+    try{
+      return downloadData(url) // returns a promise
+        .catch(e => {
+          return downloadFallbackData(url)  // returns a promise
+        })
+        .then(v => {
+          return processDataInWorker(v); // returns a promise
+        });}
+    catch(e){
+      console.error(e.message);
+
+    }     
+  }
 
 
+// primera version
 async function getProcessedData(url) {
 
     try {
